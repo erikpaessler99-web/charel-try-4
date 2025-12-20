c843930a96e36333f7732da32dad0091952d7773
erikpaessler99-web <erik.paessler99@gmail.com>
2025-12-20 17:24:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ export const CONFIG = {
   ROOM_HEIGHT: 15,
   
   // Avatar settings
-  AVATAR_COUNT: 600, // Packed club feel - 5x density
+  AVATAR_COUNT: 6000, // Packed club feel - 5x density
   AVATAR_BOUNCE_SPEED: 1.8,
   AVATAR_SWAY_SPEED: 1.2,
   AVATAR_BOUNCE_HEIGHT: 0.15, // More subtle, realistic bounce
@@ -39,3 +39,4 @@ export const CONFIG = {
     0x0080ff  // Blue
   ]
 };
+
